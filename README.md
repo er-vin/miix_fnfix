fdf8279e401ac34a82574267860e1e0e654f4695
Kevin Ottens <ervin@ipsquad.net>
2018-07-26 17:08:45 +0200

    Add script and udev rules

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # miix_fnfix
 
 Tiny script allowing to force fnlock mode on the Lenovo MIIX keyboard
+
+Two files are provided:
+ * miix_fnfix.py a python3 script depending on pyusb which forces the fnlock
+   mode on the keyboard;
+ * 99-miix.rules a UDEV rules to install in /etc/udev/rules.d to have fnlock
+   applied on startup and when the keyboard is plugged (it assumes
+   miix_fnfix.py is copied to the /root/ directory and executable, if that's
+   not the case adjust the rule file)
+
---
+++ miix_fnfix.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python3
+
+from argparse import ArgumentParser
+
+import usb.core
+import usb.util
+
+reattach_needed = False
+
+def claim_device(device, interface):
+    global reattach_needed
+    if device.is_kernel_driver_active(interface):
+        reattach_needed = True
+        device.detach_kernel_driver(interface)
+
+    usb.util.claim_interface(device, interface)
+
+def release_device(device, interface):
+    usb.util.release_interface(device, interface)
+
+    if reattach_needed:
+        device.attach_kernel_driver(interface)
+
+if __name__ == "__main__":
+    arg_parser = ArgumentParser(description="A tool for forcing fnlock mode on Lenovo MIIX keyboards")
+    arg_parser.add_argument("-u", "--unlock", action='store_true', help="Unlocks")
+    args = arg_parser.parse_args()
+
+    # Find the MIIX keyboard
+    device = usb.core.find(idVendor=0x17ef, idProduct=0x60bb)
+    if device is None:
+        raise ValueError('No MIIX keyboard attached to this system')
+
+    interface = 0x01
+
+    claim_device(device, interface)
+
+    value = 0x02
+    if (args.unlock):
+        value = 0x01
+
+    try:
+        device.ctrl_transfer(0x21, 0x09, 0x0209, interface, bytes([0x09, 0x54, value]))
+    except:
+        release_device(device, interface)
+        raise
+
+    release_device(device, interface)
+

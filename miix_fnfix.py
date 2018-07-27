0c4cb75d0c121510071608b3f79486e12f4b63ba
Kevin Ottens <ervin@ipsquad.net>
2018-07-27 10:07:38 +0200

    Add missing LICENSE file

--- miix_fnfix.py
+++ miix_fnfix.py
@@ -1,4 +1,12 @@
 #! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# Copyright 2018 Kevin Ottens <ervin@ipsquad.net>
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#
 
 from argparse import ArgumentParser
 
